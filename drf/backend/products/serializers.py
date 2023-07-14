e430a800885ac0a7dd52e6ac13df295995bffd37
Thouseef <thousithouseef731@gmail.com>
2023-07-14 20:25:56 +0530

    drf practice

--- drf/backend/products/serializers.py
+++ drf/backend/products/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    url = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields = [
+            'url', 
             'pk',   
             'title',
             'content',
@@ -13,6 +16,12 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+        
+    def get_url(self,obj):
+        request = self.context.grt("request")
+        if request is None:
+            return None
+        return reverse("product-detail",kwargs={"pk":obj.pk},request=request)
         
     def get_my_discount(self,obj):
         if not hasattr(obj,'id'):
--- drf/backend/products/urls.py
+++ drf/backend/products/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.product_list_create_view),
+    path('',views.product_list_create_view,name='product-list'),
     path('<int:pk>/update/',views.product_update_view), 
     path('<int:pk>/delete/',views.product_destroy_view), 
-    path('<int:pk>/',views.product_detail_view), 
+    path('<int:pk>/',views.product_detail_view,name='product-detail'), 
 ]
  
